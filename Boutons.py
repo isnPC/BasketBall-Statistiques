fd9e587b3fd5dae6e82fd0fdd73ef09002373d6f
Marc <mamouroux05@gmail.com>
2020-04-07 15:54:54 +0200

    Coordonnées souris / boutons (sans placements)

---
+++ Boutons.py
@@ -0,0 +1,42 @@
+﻿from tkinter import*
+fenetre = Tk()
+
+terrain = Canvas(fenetre,width=600,height=500,background="grey",bd=8)
+
+tir_raté = Button(fenetre).pack()
+tir_réussi = Button(fenetre).pack()
+ballon_perdu = Button(fenetre).pack()
+ballon_récupéré = Button(fenetre).pack()
+rebond_offensif = Button(fenetre).pack()
+rebond_defensif = Button(fenetre).pack()
+faute_subie = Button(fenetre).pack()
+faute = Button(fenetre).pack()
+lancer_réussi = Button(fenetre).pack()
+lancer_raté = Button(fenetre).pack()
+passe_décisive = Button(fenetre).pack()
+
+joueur_1 = Button(fenetre).pack()
+joueur_2 = Button(fenetre).pack()
+joueur_3 = Button(fenetre).pack()
+joueur_4 = Button(fenetre).pack()
+joueur_5 = Button(fenetre).pack()
+joueur_6 = Button(fenetre).pack()
+joueur_7 = Button(fenetre).pack()
+joueur_8 = Button(fenetre).pack()
+joueur_9 = Button(fenetre).pack()
+joueur_10 = Button(fenetre).pack()
+
+joueur_visiteur_1 = Button(fenetre).pack()
+joueur_visiteur_2 = Button(fenetre).pack()
+joueur_visiteur_3 = Button(fenetre).pack()
+joueur_visiteur_4 = Button(fenetre).pack()
+joueur_visiteur_5 = Button(fenetre).pack()
+joueur_visiteur_6 = Button(fenetre).pack()
+joueur_visiteur_7 = Button(fenetre).pack()
+joueur_visiteur_8 = Button(fenetre).pack()
+joueur_visiteur_9 = Button(fenetre).pack()
+joueur_visiteur_10 = Button(fenetre).pack()
+
+
+
+
---
+++ Coordonées souris.py
@@ -0,0 +1,7 @@
+﻿def pointeur(event):
+    """ recupere les coordonnées du clic de souris sur le canvas """
+    global posX, posY
+    affCoord.configure(text = "X=" + str(event.x) +", Y =" + str(event.y))
+    posX = event.x
+    posY = event.y
+    can.bind("<Button-1>", pointeur)
